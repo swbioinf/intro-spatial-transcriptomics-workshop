b66b373024b4bdcc431c5b79ffdbf288772f0373
swbioinf <s.williams@qcif.edu.au>
2025-09-26 05:54:25 +0000

    underscore fix

--- scripts/prep_dataset.R
+++ scripts/prep_dataset.R
@@ -33,17 +33,26 @@ if (! file.exists(seurat_file_00_raw_subset)) {
     so.raw.full[[fov_name]] <- NULL
     so.raw.full[[gsub("_","-", fov_name)]] <- fov_obj
 
+    # adnd the names
+    so.raw.full$orig.ident <- gsub("_",".", so.raw.full$orig.ident )
+
+
   }
   names(so.raw.full@images)
 
 
   so.raw.subset <- so.raw.full [, so.raw.full$group %in% c("CD", "HC") & so.raw.full@meta.data$fov <= 4]
+
+
+  # split and merge, to ensure the . names propagate better.
+  so.raw.subset <- JoinLayers(so.raw.subset)
+  so.raw.subset <- split(so.raw.subset, f = so.raw.subset$orig.ident)
+
+  so.raw.subset[["negprobes"]] <- JoinLayers(so.raw.subset[["negprobes"]])
+  so.raw.subset[["negprobes"]] <- split(so.raw.subset[["negprobes"]], f = so.raw.subset$orig.ident)
+
   saveRDS(  so.raw.subset, seurat_file_00_raw_subset)
 
 }
-
-
-
-
 
 
